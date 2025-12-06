d470d8d5f1f08d26e93acd16f885e0f3c6853c04
Kevin Ashworth <kevinashworth@yahoo.com>
2025-12-06 10:59:16 -0800

    End of Step 11

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -23,6 +23,13 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   };
 };
 
+async function deletePost(id: string): Promise<void> {
+  await fetch(`/api/post/${id}`, {
+    method: "DELETE",
+  });
+  await Router.push("/");
+}
+
 async function publishPost(id: string): Promise<void> {
   await fetch(`/api/publish/${id}`, {
     method: "PUT",
@@ -50,6 +57,9 @@ const Post: React.FC<PostProps> = (props) => {
         <ReactMarkdown children={props.content} />
         {!props.published && userHasValidSession && postBelongsToUser && (
           <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
+        {userHasValidSession && postBelongsToUser && (
+          <button onClick={() => deletePost(props.id)}>Delete</button>
         )}
       </div>
       <style jsx>{`
